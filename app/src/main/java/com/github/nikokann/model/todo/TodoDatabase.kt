77456958046a4371d38969ceee003672d4109da9
nikokann <v22157vv@aitech.ac.jp>
2022-06-26 19:32:26 +0900

    画面遷移

--- app/src/main/java/com/github/nikokann/model/todo/TodoDatabase.kt
+++ app/src/main/java/com/github/nikokann/model/todo/TodoDatabase.kt
@@ -3,7 +3,7 @@ package com.github.nikokann.model.todo
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [Todo::class], version = 1)
+@Database(entities = [Todo::class], version = 1, exportSchema = false)
 abstract class TodoDatabase: RoomDatabase() {
     abstract fun todoDAO():TodoDAO
 }
