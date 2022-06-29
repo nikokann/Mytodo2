497437ed6364fe62eacdb39c3fb224a3280343b9
nikokann <v22157vv@aitech.ac.jp>
2022-06-29 18:24:19 +0900

    gladle関係の修復

--- app/src/main/java/com/github/nikokann/page/create/create/CreateTodoFragment.kt
+++ app/src/main/java/com/github/nikokann/page/create/create/CreateTodoFragment.kt
@@ -1,11 +1,67 @@
 package com.github.nikokann.page.create.create
 
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.github.nikokann.R
+import com.github.nikokann.databinding.CreateTodoFragmentBinding
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class CreateTodoFragment: Fragment(R.layout.create_todo_fragment) {
     private val vm: CreateTodoViewModel by viewModels()
+
+    private var _binding: CreateTodoFragmentBinding? = null
+    private val binding: CreateTodoFragmentBinding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        this._binding = CreateTodoFragmentBinding.bind(view)
+
+        vm.errorMassege.observe(viewLifecycleOwner) { msg ->
+            if (msg.isEmpty()) return@observe
+
+            Snackbar.make(requireView(), msg, Snackbar.LENGTH_SHORT).show()
+            vm.errorMassege.value = ""
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        this._binding = null
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_create, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.action_save -> {
+                save()
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+
+    }
+
+    private fun save() {
+        val title = binding.titleEdit.text.toString()
+        val detail = binding.detailEdit.text.toString()
+
+        vm.save(title, detail)
+    }
+
 }
--- app/src/main/java/com/github/nikokann/page/create/create/CreateTodoViewModel.kt
+++ app/src/main/java/com/github/nikokann/page/create/create/CreateTodoViewModel.kt
@@ -1,6 +1,20 @@
 package com.github.nikokann.page.create.create
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.github.nikokann.repository.todo.Todorepository
 
-class CreateTodoViewModel: ViewModel() {
+class CreateTodoViewModel(
+    private val repo: Todorepository
+): ViewModel() {
+    val errorMassege = MutableLiveData<String>()
+    fun save(title: String, detail: String) {
+        //タイトルが空っぽだったらエラーメッセージを出す
+        if (title.trim().isEmpty()) {
+            errorMassege.value = "Please input title"
+            return
+        }
+        //リポジトリ経由で実際の保存処理を行う
+
+    }
 }
